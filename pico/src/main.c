0cb52499f2bed739db8663ac3ccec8694bca86b8
Dansheez <rysiek77a@gmail.com>
2024-09-30 22:27:41 +0200

    Implemented serial communication via tinyusb

--- pico/src/main.c
+++ pico/src/main.c
@@ -1,75 +1,34 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/dma.h"
-#include "hardware/pio.h"
 
-// Data will be copied from src to dst
-const char src[] = "Hello, world! (from DMA)";
-char dst[count_of(src)];
+#include "tusb.h"
+#include "pico_communication.h"
 
-#include "blink.pio.h"
-
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
-    pio_sm_set_enabled(pio, sm, true);
-
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
-
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
-}
-
-
+// TODO: move this to config
+#define BUFFER_SIZE 64
+uint8_t* receive_buffer;
+uint8_t* send_buffer;
+uint16_t received_bytes = 0;
 
 int main()
 {
+    receive_buffer = init_buffer(BUFFER_SIZE);
+    send_buffer = init_buffer(BUFFER_SIZE);
+
     stdio_init_all();
-
-    // Get a free channel, panic() if there are none
-    int chan = dma_claim_unused_channel(true);
+    tusb_init();
     
-    // 8 bit transfers. Both read and write address increment after each
-    // transfer (each pointing to a location in src or dst respectively).
-    // No DREQ is selected, so the DMA transfers as fast as it can.
-    
-    dma_channel_config c = dma_channel_get_default_config(chan);
-    channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
-    channel_config_set_read_increment(&c, true);
-    channel_config_set_write_increment(&c, true);
-    
-    dma_channel_configure(
-        chan,          // Channel to be configured
-        &c,            // The configuration we just created
-        dst,           // The initial write address
-        src,           // The initial read address
-        count_of(src), // Number of transfers; in this case each is 1 byte.
-        true           // Start immediately.
-    );
-    
-    // We could choose to go and do something else whilst the DMA is doing its
-    // thing. In this case the processor has nothing else to do, so we just
-    // wait for the DMA to finish.
-    dma_channel_wait_for_finish_blocking(chan);
-    
-    // The DMA has now copied our text from the transmit buffer (src) to the
-    // receive buffer (dst), so we can print it out from there.
-    puts(dst);
-
-    // PIO Blinking example
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blink_program);
-    printf("Loaded program at %d\n", offset);
-    
-    #ifdef PICO_DEFAULT_LED_PIN
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
-    #else
-    blink_pin_forever(pio, 0, offset, 6, 3);
-    #endif
-    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
-
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        tud_task(); // USB device task
+        if (received_bytes > 0) {
+            send_buffer = receive_buffer;
+            send_data(send_buffer, received_bytes);
+            received_bytes = 0;
+        } 
+       
     }
+}
+
+void tud_cdc_rx_cb(uint8_t itf) {
+    // This callback is called when data is received
+    received_bytes = receive_data(receive_buffer, BUFFER_SIZE);
 }
