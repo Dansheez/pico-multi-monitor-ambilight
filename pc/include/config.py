99aa27f41f7a8e5303834897277d5373e9ea41cf
Dansheez <rysiek77a@gmail.com>
2024-10-04 20:51:33 +0200

    Added screen capture as an asynchronous task

--- pc/include/config.py
+++ pc/include/config.py
@@ -5,6 +5,9 @@ MONITOR_COUNT = 1
 # Width of an area to calculate dominant color (in pixels).
 AREA_WIDTH = 100
 
+# Target frequency of capturing the screen (in Hz).
+TARGET_FPS = 30
+
 # **LED CONFIGURATION**
 # Specified order in which the LED strips are connected to each side (left, bottom, right, top) for each monitor.
 # 0 - first
--- pc/main.py
+++ pc/main.py
@@ -1,22 +1,31 @@
 import mss
+import asyncio
+import time
+
 import include.capture_screen as capture_screen
+import include.config as config
 
-def main():
-    #setup
+
+async def capture_screen_job(sct: mss, delay: float):
+    while True:
+        start_time = time.time()
+        message = capture_screen.compose_message_from_screen_data(sct)
+        print(f"{message}")
+        await asyncio.sleep(max(0, delay - (time.time() - start_time)))
+
+async def main():
     capture_screen.get_screen_capture_config()
-
     # main loop
-    with mss.mss() as sct:
-        try:
-            while True:
-                message = capture_screen.compose_message_from_screen_data(sct)
-                print(message)
-        except KeyboardInterrupt:
-            print("Exiting...")
-        except Exception as e:
-            print(e)
-        finally:
-            sct.close()
+    try:
+        with mss.mss() as sct:
+            delay = 1/config.TARGET_FPS
+            await capture_screen_job(sct, delay) 
+    except KeyboardInterrupt:
+        print("Exiting...")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        sct.close()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
