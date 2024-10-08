a2e6e64a04886409fd00e030ac9a5c956f78344b
Dansheez <rysiek77a@gmail.com>
2024-10-08 13:39:54 +0200

    Simplified loop function and updated docstrings

--- pc/include/pc_communication.py
+++ pc/include/pc_communication.py
@@ -24,6 +24,9 @@ def receive_data_via_serial_connection(serial_port: serial)->list:
         return []
 
 def get_max_data_length_in_block()->int:
+    """How many data bytes can be sent in one block. 
+    CDC_MAX_BLOCK_SIZE - (trailer, blockID, checksum, payload_length)
+    """
     from math import floor
     CDC_MAX_BLOCK_SIZE = 64 # in bytes
     avaliable_data_size_in_block = CDC_MAX_BLOCK_SIZE - 4 # max size - (trailer, blockID, checksum, payload_length)  
@@ -31,7 +34,21 @@ def get_max_data_length_in_block()->int:
     max_data_length = max_leds_in_one_block*(4 if config.IS_RGBW else 3) # max data length
     return max_data_length
 
-def compose_data_into_packet(data: list, blockID: int = 0x00):
+def create_data_block(data: list, blockID: int = 0x00)->bytearray:
+    """Block consists of:
+    - blockID [1 byte]
+    - payload_length [1 byte] (only length of data, not including blockID, checksum and trailer)
+    - data [variable length]
+    - checksum [1 byte] (sum of all previous bytes modulo 256)
+    - trailer [1 byte]
+
+    Args:
+        data (list): data to be packed into a block
+        blockID (int, optional): ID of the block. Defaults to 0x00.
+
+    Returns:
+        bytearray: message block ready to be sent
+    """
     message = bytearray()
     message.append(blockID) # blockID [1 byte]
     message.append(len(data)) # payload_length [1 byte]
@@ -41,37 +58,44 @@ def compose_data_into_packet(data: list, blockID: int = 0x00):
     message.append(0x55) # trailer [1 byte]
     return message
 
+def wait_for_response_and_interpret(serial_port: serial)->int:
+    """
+    Returns:
+        int:  0 - success, 1 - error, 2 - timeout 
+    """
+    start_time = time()
+    while True:
+        response = receive_data_via_serial_connection(serial_port)
+        if response:
+            # print(f"Response received: {response}")
+            if 'ACK' in response: return 0
+            elif 'ERROR' in response: return 1
+        elif time() - start_time > config.TIMEOUT_DURATION: # if timeout is reached
+            return 2
+
 max_data_length = get_max_data_length_in_block()
-def data_send_loop(serial_port: serial, screen_data: list):
+def data_send_loop(serial_port: serial, screen_data: list)->int:
+    """Sends the whole packet of data in one or multiple blocks if it is too large. 
+    Waits for the response after each block until the timeout is reached.
+
+    Args:
+        serial_port (serial): serial port to send the data
+        screen_data (list): raw data to be split, packed into blocks and sent
+
+    Returns:
+        int:  0 - success, 1 - error, 2 - timeout
+    """
     index_counter = 0
     blockID = 0x00
     while index_counter<len(screen_data):
-        # print(f"Sending blockID: {blockID}")
         try:
            splitted_data = screen_data[index_counter:index_counter+max_data_length]
         except IndexError:
            splitted_data = screen_data[index_counter:] 
-        splitted_data_packet = compose_data_into_packet(splitted_data, blockID)
+        splitted_data_packet = create_data_block(splitted_data, blockID)
         send_data_via_serial_connection(serial_port, splitted_data_packet)
-        # Start the timeout timer
-        start_time = time()
-        # print(f"Index counter: [{index_counter}:{index_counter+len(splitted_data)}]")
-
-        # Wait for response until the timeout is reached
-        while True:
-            response = receive_data_via_serial_connection(serial_port)
-            # TODO: make intrepret response function
-            if response: # if any response is received
-                # print(f"Response received: {response}")
-                if 'ACK' in response:
-                   blockID += 1
-                   index_counter += len(splitted_data)
-                   break # exit the timeout loop
-                elif 'ERROR' in response:
-                   return 1 # exit the function with error code
-                else:
-                    pass # unidentified response, ignore
-            elif time() - start_time > config.TIMEOUT_DURATION: # if timeout is reached
-                # print(f"No response received within timeout period of {config.TIMEOUT_DURATION} seconds")
-                return 2 # exit the function with error code
+        if(ret:=wait_for_response_and_interpret(serial_port)):
+            return ret # error code
+        blockID += 1
+        index_counter += len(splitted_data)
     return 0 # succesfully sent all data
